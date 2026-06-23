be76995e17d526fc535722bc2901338bb992cc01
Bhawandeep Singh <bhawandeepsingh976@gmail.com>
2026-06-23 15:23:07 +0530

    email

--- backend/src/config/mail.js
+++ backend/src/config/mail.js
@@ -1,5 +1,7 @@
 const nodemailer = require("nodemailer");
 
+console.log("EMAIL_USER:", process.env.EMAIL_USER);
+console.log("EMAIL_PASS:", process.env.EMAIL_PASS ? "FOUND" : "MISSING");
 const transporter =
 nodemailer.createTransport({
 
